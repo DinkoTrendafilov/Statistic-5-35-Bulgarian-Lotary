dced95bfd265a6eb5a2dcad12a51da86f69d65a9
Dinko Delchev Trendafilov <32400715+DinkoTrendafilov@users.noreply.github.com>
2024-11-03 19:08:18 +0200

    Update 5_35.py

--- 5_35_statistic/5_35.py
+++ 5_35_statistic/5_35.py
@@ -1,37 +1,31 @@
 import random
 
 numbers_of_rotation = int(input("Моля въведете броя на тегленията на числата: "))
-# Статистика за познатите числа
+
 keys_list = [num for num in range(1, 6)]
 value_list = [0 for num in range(1, 6)]
 
 my_dictionary = dict(zip(keys_list, value_list))
 my_list = []
-
-# Статистика за числата от 1 до 35, които да се падали, от страна на тото сървъра
+total_number_of_combination = (35 * 34 * 33 * 32 * 31) / (5 * 4 * 3 * 2 * 1)
 
 toto_keys_list = [num for num in range(1, 36)]
 toto_value_list = [0 for num in range(1, 36)]
 
 toto_dictionary = dict(zip(keys_list, value_list))
 toto_list = []
+counter = 0
 
 for _ in range(numbers_of_rotation):
     result = 0
-    toto_server_numbers = []
-    user_numbers = []
-    for _ in range(25):
-        pc_number = random.randint(1, 35)
-        if pc_number not in toto_server_numbers:
-            toto_server_numbers.append(pc_number)
-        if len(toto_server_numbers) == 5:
-            break
-    for _ in range(25):
-        pc_number = random.randint(1, 35)
-        if pc_number not in user_numbers:
-            user_numbers.append(pc_number)
-        if len(user_numbers) == 5:
-            break
+    toto_server_numbers = [num for num in range(1, 36)]
+    user_numbers = [num for num in range(1, 36)]
+
+    random.shuffle(toto_server_numbers)
+    random.shuffle(user_numbers)
+
+    toto_server_numbers = toto_server_numbers[:5]
+    user_numbers = user_numbers[:5]
 
     for num1 in toto_server_numbers:
         toto_list.append(num1)
@@ -39,9 +33,14 @@ for _ in range(numbers_of_rotation):
             if num1 == num2:
                 result += 1
     if result == 5:
-        print(f"Поздравления, вие познахте и 5те числа!!!, а именно: {toto_server_numbers}")
-    my_list.append(result)
+        counter += 1
+        print(f"#{counter:_} Поздравления, вие познахте и 5те числа!!!, а именно: {toto_server_numbers}")
 
+    my_list.append(result)
+print()
+print(f"Вашият брой 5-ци: [{counter:_}] | от {numbers_of_rotation:_} броя тегления се очаква "
+      f"[{(numbers_of_rotation / total_number_of_combination):.2f}] броя 5-ци")
+print()
 for element in my_list:
     if element not in my_dictionary:
         my_dictionary[element] = 1
@@ -50,7 +49,6 @@ for element in my_list:
 
 sorted_dict = dict(sorted(my_dictionary.items(), key=lambda x: (-x[1], x[0])))
 
-# _____________________________________
 for element in toto_list:
     if element not in toto_dictionary:
         toto_dictionary[element] = 1
@@ -60,19 +58,13 @@ for element in toto_list:
 toto_sorted_dict = dict(sorted(toto_dictionary.items(), key=lambda x: (-x[1], x[0])))
 
 print(f"{'-' * 113}")
-total_number_of_combination = (35 * 34 * 33 * 32 * 31) / (5 * 4 * 3 * 2 * 1)
-total_number_of_combination = "{:,}".format(total_number_of_combination).replace(",", "_")
-print(f"Общият брой на всички комбинации: {total_number_of_combination}")
+print(f"Общият брой на всички комбинации: {total_number_of_combination:_}")
 print()
 for number, value in sorted_dict.items():
-    value = "{:,}".format(value).replace(",", "_")
-    print(f"Познати числа: {number:02d} -> Честота на случване: {value}")
+    print(f"Познати числа: {number:02} -> Честота на случване: {value:_}")
 
 print(f"{'-' * 113}")
 print()
-
 for number, value in toto_sorted_dict.items():
-    value = "{:,}".format(value).replace(",", "_")
-    print(f"Числото: {number:02d} -> Честота на случване: {value}")
-
+    print(f"Числото: {number:02} -> Честота на случване: {value:_}")
 print(f"{'-' * 113}")
